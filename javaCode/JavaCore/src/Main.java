e9ede41dbb8b945839c46af8f215233951b68b5b
prasanna_dixit <dixit.prasanna8@gmail.com>
2023-10-25 18:56:23 +0530

    updating ntoes

--- javaCode/JavaCore/src/Main.java
+++ javaCode/JavaCore/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    public Main() {
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x = 257;
+        byte y = (byte) x;
+        System.out.println(y);
+        System.out.println(x%127);
+        
     }
 }
